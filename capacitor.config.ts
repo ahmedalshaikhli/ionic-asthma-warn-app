cd306242953f12efe8ec7f1b12f6c868ddd443ca
Al Shaikhli <ahmed.al.shaikhli@accenture.com>
2023-05-02 15:32:33 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-asthma-warn-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
